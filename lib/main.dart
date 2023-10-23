9cb476f3e3fb2187396d330158ecc26992a80919
Ahmad Fathan Aqil Faiq <93438475+Fthnn@users.noreply.github.com>
2023-10-23 14:56:09 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:belanja/pages/home_pages.dart';
+import 'package:belanja/pages/item_pages.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      initialRoute: '/',
+      routes: {'/': (context) => HomePage(), '/item': (context) => ItemPage()},
+    ),
+  );
+}
---
+++ lib/pages/item_pages.dart
@@ -0,0 +1,39 @@
+import 'package:belanja/models/items.dart';
+import 'package:flutter/material.dart';
+
+class ItemPage extends StatelessWidget {
+  const ItemPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final itemArgs = ModalRoute.of(context)!.settings.arguments as Item;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shopping List'),
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(15),
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                itemArgs.name.toString(),
+                style: const TextStyle(fontSize: 24),
+              ),
+              const SizedBox(width: 6),
+              const Text(
+                'with',
+                style: TextStyle(fontSize: 18),
+              ),
+              const SizedBox(width: 6),
+              Text(
+                itemArgs.price.toString(),
+                style: const TextStyle(fontSize: 18),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
